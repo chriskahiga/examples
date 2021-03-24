26acd1d106a76f9de83530805f918828398d1e39
chriskahiga <46884078+chriskahiga@users.noreply.github.com>
2021-03-24 17:26:44 +0300

    Update config.py

--- vote-frontend/config.py
+++ vote-frontend/config.py
@@ -3,5 +3,4 @@ import os
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
-forwarded_allow_ips = '*'
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+bind = "0.0.0.0:8080"
